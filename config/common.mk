903f40a05c6b58c50edbf48d474273dee26d4e4e
AngryLiCat <532007193@qq.com>
2015-04-02 17:24:03 +0800

    remove Term.apk
    Change-Id: I22fd5794a5d6f0e677a023a088887cfadb55b3d1

--- config/common.mk
+++ config/common.mk
@@ -217,9 +217,9 @@ PRODUCT_PACKAGES += \
     su
 
 # Terminal Emulator
-PRODUCT_COPY_FILES +=  \
-    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+#PRODUCT_COPY_FILES +=  \
+#    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
+#    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm5.so:system/lib/libjackpal-androidterm5.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
